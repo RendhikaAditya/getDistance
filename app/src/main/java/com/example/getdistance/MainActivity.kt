ebed8b810758ec504581c8b165a2c67a3488d67a
RendhikaAditya <aditya0909.a3@gmail.com>
2021-02-23 22:55:06 +0700

    asdas

---
+++ app/src/main/java/com/example/getdistance/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.getdistance
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+
+class MainActivity : AppCompatActivity() {
+    var sydney: LatLng = LatLng((-34).toDouble(), 151.0)
+    var Brisbane: LatLng = LatLng(-27.470125, 153.021072)
+    var distance: Double? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        distance = SphericalUtil.computeDistanceBetween(sydney, Brisbane)
+
+        Toast.makeText(this, "Distance between Sydney and Brisbane is \n " + String.format("%.2f", distance!! / 1000) + "km", Toast.LENGTH_SHORT).show();
+    }
+}
